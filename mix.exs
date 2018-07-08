c8507986e4b8c5f0b44ad3dee6431a2bbde27ece
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2018-07-08 21:00:58 +0100

    🔥  Remove unecessary logger app

--- mix.exs
+++ mix.exs
@@ -18,7 +18,6 @@ defmodule MapBot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
       mod: {MapBot.Application, []}
     ]
   end
