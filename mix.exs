23434a2984f811db0298fef4bb3886edaa6a30d5
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2018-10-22 15:34:29 +0100

    ⬇️  Downgrade to elixir 1.6 for backward compatibility

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule MapBot.MixProject do
       app: :map_bot,
       version: "1.1.0",
       package: package(),
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       source_url: "https://github.com/vnegrisolo/map_bot",
