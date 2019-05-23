c96b7688183844fa933996a24ebe084f457d80bf
Miguel Pinto <migpin@gmail.com>
2019-05-23 19:29:42 +0100

    🐛 Move my_app.ex to priv folder
    It is required by a moduledoc and since it was located inside
    the test folder, it was not included in the Hex package.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,9 @@
 {:ok, _pid} = MapBot.Sequence.start_link()
 ExUnit.start()
 
-{:ok, files} = File.ls("./test/support")
-files |> Enum.map(&"support/#{&1}") |> Enum.each(&Code.require_file(&1, __DIR__))
+support_path = :map_bot |> :code.priv_dir() |> Path.join("support")
+{:ok, files} = support_path |> File.ls()
+
+files
+|> Enum.map(&Path.join(support_path, &1))
+|> Enum.each(&Code.require_file(&1, __DIR__))
